303903914b74470e960fef5dc5567bc6474fc987
D'Ippoliti Francesco <francesco.dippoliti@gtech.com>
2017-11-24 15:19:25 +0100

    Version 1.0-snapshot

---
+++ src/main/java/com/igt/interactive/jmxclient/Application.java
@@ -0,0 +1,4 @@
+package com.igt.interactive.jmxclient;
+
+public class Application {
+}
---
+++ src/main/java/com/igt/interactive/jmxclient/JmxClient.java
@@ -0,0 +1,43 @@
+package com.igt.interactive.jmxclient;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Client {
+
+    String mbeanname;
+    String mbeanattr;
+    String mbeanvalue;
+
+    public Client(String mbeanname, String mbeanattr, String mbeanvalue ) {
+
+        this.mbeanattr = mbeanattr;
+        this.mbeanname = mbeanname;
+        this.mbeanvalue = mbeanvalue;
+    }
+
+    public String getMbeanname() {
+        return mbeanname;
+    }
+
+    public String getMbeanattr() {
+        return mbeanattr;
+    }
+
+    public String getMbeanvalue() {
+        return mbeanvalue;
+    }
+
+
+
+}
